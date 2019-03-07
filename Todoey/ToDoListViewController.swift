dafd162ba1ec2017a104fa0c1c4134fda03151ba
joelwongschow <joelwongschow@gmail.com>
2019-03-06 18:38:11 -0800

    Data now persistant

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -12,9 +12,15 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogrogon"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -50,6 +56,7 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append((alert.textFields?[0].text)!)
             self.addNewestItemToTableView()
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
         }
         
         alert.addAction(action)
